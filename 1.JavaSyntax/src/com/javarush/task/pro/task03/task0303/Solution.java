d70a89fc4bfc3b4c0daf8bdf8e747bc84090cae4
Maksim Korelov <korelov@gmail.com>
2022-01-12 14:13:35 +0300

    3 lvl

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0303/Solution.java
@@ -10,10 +10,10 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int age = scanner.nextInt();
-        if (age < 18)
-            if (age >= 6)
-                System.out.println("нужно ходить в школу");
-        else
+        if (age >= 6 && age < 18) {
+            System.out.println("нужно ходить в школу");
+        } else if (age >= 18) {
             System.out.println("пора в институт");
+        }
     }
 }
