32cd1f033d0ef835343720490371290bc518b177
Maksim Korelov <korelov@gmail.com>
2022-01-19 22:19:49 +0300

    4 lvl Скорость ветра

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0417/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task04.task0417;
+
+import java.util.Scanner;
+
+/* 
+Скорость ветра
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(Math.round(a * 3.6));
+
+    }
+}
