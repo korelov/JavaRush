557bcd1922226eee6f43b94f207c6dabe32c42b1
Maksim Korelov <korelov@gmail.com>
2022-01-21 19:31:59 +0300

    6 lvl Универсальный солдат

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    public static void universalMethod(int a) {
+
+    }
+
+    public static void universalMethod(int a, int b) {
+
+    }
+
+    public static void universalMethod(int a, int b, int c) {
+
+    }
+
+    public static void universalMethod(int a, int b, int c, int d) {
+
+    }
+
+    public static void universalMethod(int a, int b, int c, int d, int f) {
+
+    }
+
+    public static void universalMethod(int a, int b, int c, int d, int f, int e, int g) {
+
+    }
+
+    public static void universalMethod(int a, int b, int c, int d, int f, int e, int g, int h) {
+
+    }
+
+    public static void universalMethod(int a, int b, int c, int d, int f, int e, int g, int h, int p) {
+
+    }
+
+    public static void universalMethod(int a, int b, int c, int d, int f, int e) {
+
+    }
+
+
+}
