049685f873cd9e5fb3dfa2edd5c0019c9904e098
Maksim Korelov <korelov@gmail.com>
2022-01-25 10:34:56 +0300

    7 lvl Все возможные типы

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte b = Byte.MAX_VALUE;
+    short s = Short.MAX_VALUE;
+    int i = Integer.MAX_VALUE;
+    long l = Long.MAX_VALUE;
+
+    float f = Float.MAX_VALUE;
+    double d = Double.MAX_VALUE;
+
+    char c = Character.MAX_VALUE;
+
+    boolean b1 = true;
+}
