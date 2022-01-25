66199382b44b918fc7994cbee64695f41723f89d
Maksim Korelov <korelov@gmail.com>
2022-01-25 10:36:17 +0300

    7 lvl Максимальное целое

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte b = Byte.MAX_VALUE;
+    short s = Short.MAX_VALUE;
+    int i = Integer.MAX_VALUE;
+    long l = Long.MAX_VALUE;
+
+    float f = Float.MAX_VALUE;
+    double d = Double.MAX_VALUE;
+
+    char c = Character.MAX_VALUE;
+
+}
