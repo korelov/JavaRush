e1ff50476ed7d7be1604bf957485c175fa0f5e10
Maksim Korelov <korelov@gmail.com>
2022-03-06 21:54:44 +0300

    7 lvl Первые объекты

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s1 = new String();
+        String s2 = new String();
+        String s3 = new String();
+        String s4 = new String();
+        String s5 = new String();
+        String s6 = new String();
+        String s7 = new String();
+        String s8 = new String();
+        String s9 = new String();
+        String s10 = new String();
+        int[] arr1 = new int[1];
+        int[] arr2 = new int[2];
+        int[] arr3 = new int[4];
+        int[] arr4 = new int[5];
+        int[] arr5 = new int[7];
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+    }
+}
