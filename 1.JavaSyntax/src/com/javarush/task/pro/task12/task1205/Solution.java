5f5b3d13feaf85df3affcc9dff037d0b09a8ef1e
Maksim Korelov <korelov@gmail.com>
2022-06-11 22:17:53 +0300

    13 lvl Метод деления

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1205/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task12.task1205;
+
+/* 
+Метод деления
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        divide(1, 0);
+        divide(-1, 0);
+        divide(0, 0);
+        divide(100, 20);
+        divide(200, 20);
+    }
+
+    public static void divide(double a, double b) {
+        if (a > 0 && b == 0) {
+            System.out.println(Double.POSITIVE_INFINITY);
+        } else if (a < 0 && b == 0) {
+            System.out.println(Double.NEGATIVE_INFINITY);
+        } else if (a == 0 && b == 0) {
+            System.out.println(Double.NaN);
+        } else {
+            System.out.println(a / b);
+        }
+    }
+}
