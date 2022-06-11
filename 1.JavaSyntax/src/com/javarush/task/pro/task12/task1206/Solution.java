5b8465645cae06cee1aaf3a2933e94d53d836d04
Maksim Korelov <korelov@gmail.com>
2022-06-11 22:32:01 +0300

    13 lvl Анализ строк

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1206/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task12.task1206;
+
+/* 
+Анализ строк
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Думаю, это будет новой фичей." +
+                "Только не говорите никому, что она возникла случайно.";
+
+        System.out.println("Количество цифр в строке : " + countDigits(string));
+        System.out.println("Количество букв в строке : " + countLetters(string));
+        System.out.println("Количество пробелов в строке : " + countSpaces(string));
+    }
+
+    public static int countDigits(String string) {
+        return string.replaceAll("[^0-9]", "").length();
+    }
+
+    public static int countLetters(String string) {
+        return string.replaceAll("[^a-zA-Zа-яА-Я\\,\\.]", "").length();
+    }
+
+    public static int countSpaces(String string) {
+        return string.replaceAll("[^\\ ]", "").length();
+    }
+}
