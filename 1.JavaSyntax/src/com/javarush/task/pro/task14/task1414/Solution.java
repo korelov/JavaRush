1cc488c41c1ae4319b3c3b5e7def80ab2029861d
Maksim Korelov <korelov@gmail.com>
2022-06-19 19:10:57 +0300

    15 lvl Готовим коктейли

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1414/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task14.task1414;
+
+/* 
+Готовим коктейли
+*/
+
+public class Solution {
+
+    public static final String OUTPUT_FORMAT = "Метод %s вызван из строки %d класса %s в файле %s.\n";
+
+    public static void main(String[] args) {
+        makeScrewdriver();
+    }
+
+    public static void printStackTrace(StackTraceElement[] stackTrace) {
+        StackTraceElement[] stackTraceElements = stackTrace;
+        for (StackTraceElement a : stackTraceElements) {
+            System.out.printf(OUTPUT_FORMAT, a.getMethodName(), a.getLineNumber(), a.getClassName(), a.getFileName());
+        }
+    }
+
+    static void makeScrewdriver() {
+        addJuice();
+    }
+
+    static void addJuice() {
+        addVodka();
+    }
+
+    static void addVodka() {
+        printStackTrace(Thread.currentThread().getStackTrace());
+    }
+}
